e58c408f7b94a408d8960c14fc5e509e57d71bbe
Vieira <tvieira@ic.ufal.br>
2021-09-14 16:46:57 -0300

    Updated results

--- src/plot-gaussian-fit.py
+++ src/plot-gaussian-fit.py
@@ -1,7 +1,6 @@
 #%%
 from utils import *
 
-#%%
 font                   = cv2.FONT_HERSHEY_PLAIN
 bottomLeftCornerOfText = (5,28)
 fontScale              = .2
@@ -10,10 +9,9 @@ lineType               = 1
 
 images = []
 for ind in range(20):
+    # ind = 1
 
-    # ind = 8
-
-    # imfilename = '../results/10-microns particles-60X/crop_centered/crop_centered_' + str(ind) +'.tif'
+    imfilename = '../results/10-microns particles-60X/crop_centered/crop_centered_' + str(ind) +'.tif'
     I = cv2.imread(imfilename, cv2.IMREAD_GRAYSCALE)
 
     new_shape = (32, 32)
@@ -25,6 +23,7 @@ for ind in range(20):
     I = I / np.sqrt(np.mean(I**2))
     images.append(I)
 
+
     # Read JSON file containing the result of the Gaussian fit.
     json_filename = '../results/10-microns particles-60X/gaussian_fit/crop_centered_gaussian_fit_'        + str(ind) +'.json'
     # Get the Gaussian parameters
@@ -32,13 +31,14 @@ for ind in range(20):
     O = [u1, u2, sig11, sig12, sig22]
 
     # # Compute the Gaussian function
-    # yHat = gaussian2D(O)
+    yHat = gaussian2D(O)
+    cv2.circle(I, (int(u1+16), int(u2+16)), int(2*sig11), I.max(), 1)
 
     # Output results
     # Plot the figure
     plt.figure()
     plt.imshow(I, cmap='gray')
-    plt.contour(yHat)
+    # plt.contour(yHat)
     plt.grid(False)
     plt.axis(False)
     plt.xlabel(f'{sig11:.02e}')
